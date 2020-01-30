bacfc5f0a5ff9d708fe6cf7b89bce77dde46923c
Tobias Rosenberg <tob.rosenberg@gmail.com>
2020-01-30 14:03:07 +0800

    clean starter and setup structure to begin

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,17 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./_header.scss"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  <header className="header"
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <div className="header__container"
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+          className="header__link"
         >
           {siteTitle}
         </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+// all below come from starter code
+import Layout from "../components/layout/layout"
+import Image from "../components/image/image"
+import SEO from "../components/seo/seo"
 
 const IndexPage = () => (
-  <Layout>
+   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -16,6 +18,8 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
+
+
 )
 
 export default IndexPage
